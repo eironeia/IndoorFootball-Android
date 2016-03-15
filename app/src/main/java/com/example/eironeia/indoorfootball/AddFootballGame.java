901adf38ff6a1a38ec9c4bce79e84800c58f3d23
Alex Eironeia <acuellor.6@gmail.com>
2016-03-15 08:16:27 +0100

    layouts createds

---
+++ app/src/main/java/com/example/eironeia/indoorfootball/AddFootballGame.java
@@ -0,0 +1,9 @@
+package com.example.eironeia.indoorfootball;
+
+/**
+ * Created by Eironeia on 15/3/16.
+ */
+public class AddFootballGame {
+
+
+}
---
+++ app/src/main/java/com/example/eironeia/indoorfootball/AddPlayer.java
@@ -0,0 +1,7 @@
+package com.example.eironeia.indoorfootball;
+
+/**
+ * Created by Eironeia on 15/3/16.
+ */
+public class AddPlayer {
+}
